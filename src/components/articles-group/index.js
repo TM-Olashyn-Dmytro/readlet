f0f167a037b5829850cc37f4407cedd88a7e29a0
Julio Cesar Ody <julio@awesomebydesign.com>
2016-03-28 10:13:30 +1100

    Deleted unused component, add articles group

---
+++ src/components/articles-group/index.js
@@ -0,0 +1,12 @@
+// Articles group component
+// ========================
+
+import React from 'react'
+import style from './index.scss'
+
+export default class ArticlesGroup extends React.Component {
+  render() {
+    return <div className={style.ArticlesGroup}>
+    </div>
+  }
+}
