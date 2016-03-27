6dd9a2107c2e077051323f1f6c2a728dbb270aa5
Julio Cesar Ody <julio@awesomebydesign.com>
2016-03-28 10:28:35 +1100

    Add data faker, fake data. Iterate-render

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,10 +3,19 @@
 
 import React from 'react'
 import style from './index.scss'
+import ArticlesGroup from '../articles-group'
+import faker from 'faker'
+
+let mockArticles = [
+  { title: faker.lorem.words(), text: faker.lorem.paragraphs() },
+  { title: faker.lorem.words(), text: faker.lorem.paragraphs() },
+  { title: faker.lorem.words(), text: faker.lorem.paragraphs() }
+]
 
 export default class App extends React.Component {
   render() {
     return <div className={style.App}>
+      <ArticlesGroup articles={mockArticles} />
     </div>
   }
 }
--- src/components/articles-group/index.js
+++ src/components/articles-group/index.js
@@ -3,10 +3,18 @@
 
 import React from 'react'
 import style from './index.scss'
+import Article from '../article'
 
 export default class ArticlesGroup extends React.Component {
   render() {
     return <div className={style.ArticlesGroup}>
+      {this.props.articles.map(article => {
+        return <Article article={article} />
+      })}
     </div>
   }
+}
+
+ArticlesGroup.propTypes = {
+  articles: React.PropTypes.array.isRequired
 }
