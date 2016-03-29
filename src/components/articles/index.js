07ebd8c94ed0b243c614611b229e70573f3b41d4
Julio Cesar Ody <julio@awesomebydesign.com>
2016-03-29 16:27:28 +1100

    Move logic to a model

---
+++ src/components/articles/index.js
@@ -0,0 +1,24 @@
+// Article
+// =======
+
+import faker from 'faker'
+
+function randomParagraphCount() {
+  return Math.random() * (13 - 3) + 3
+}
+
+function randomArticle() {
+  return {
+    id: faker.random.number(),
+    title: faker.lorem.words(),
+    text: faker.lorem.paragraphs(randomParagraphCount())
+  }
+}
+
+let articles = [
+  randomArticle(),
+  randomArticle(),
+  randomArticle()
+]
+
+export default articles
