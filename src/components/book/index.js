7001b6183f1b913f877d4afd3b024555edf521dc
Julio Cesar Ody <julio@awesomebydesign.com>
2016-04-01 10:43:29 +1100

    Wire up the events and helpers

--- src/components/book/index.js
+++ src/components/book/index.js
@@ -4,17 +4,26 @@
 import React from 'react'
 import style from './index.scss'
 import Sheet from '../sheet'
+import articles from '../articles'
+import EventBus from '../event-bus'
 
 export default class Book extends React.Component {
+  constructor(options = {}) {
+    super(options)
+    this.state = { articles: [] }
+  }
+
+  componentDidMount() {
+    EventBus.subscribe('article:add', () => {
+      this.setState({ articles: articles })
+    })
+  }
+
   render() {
     return <div className={style.Book}>
-      {this.props.articles.map(article => {
+      {this.state.articles.map(article => {
         return <Sheet key={article.id} article={article} />
       })}
     </div>
   }
-}
-
-Book.propTypes = {
-  articles: React.PropTypes.array.isRequired
 }
