cd704e3ebacfbfa986c9bcaf9e7f9508f27df261
Julio Cesar Ody <julio@awesomebydesign.com>
2016-04-01 10:42:34 +1100

    Let data be controlled by sourcing the module directly

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -5,13 +5,13 @@ import React from 'react'
 import style from './index.scss'
 import Book from '../book'
 import SearchForm from '../search-form'
-import articles from '../articles'
+import EventBus from '../event-bus'
 
 export default class App extends React.Component {
   render() {
     return <div className={style.App}>
       <SearchForm />
-      <Book articles={articles} />
+      <Book />
     </div>
   }
 }
