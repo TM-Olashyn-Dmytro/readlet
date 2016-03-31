24df29706280d188a7cff48c7575d7ba635adbdd
Julio Cesar Ody <julio@awesomebydesign.com>
2016-04-01 10:43:48 +1100

    As in now

--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -2,6 +2,28 @@
 // =======
 
 import faker from 'faker'
+import {
+  fetchPage,
+  pageToArticle,
+  performSearch,
+  searchResultsToArticles
+} from '../wikipedia-api'
+import EventBus from '../event-bus'
+
+EventBus.subscribe('article:fetch', onFetchArticle)
+EventBus.subscribe('search:perform', onSearchPerform)
+
+function onFetchArticle(name) {
+  fetchArticle(name)
+}
+
+function onSearchPerform(query) {
+  search(query, {
+    success: (articles) => {
+      EventBus.broadcast('search:success', articles)
+    }
+  })
+}
 
 function randomParagraphCount() {
   return Math.random() * (13 - 3) + 3
@@ -15,10 +37,23 @@ export function randomArticle() {
   }
 }
 
-let articles = [
-  randomArticle(),
-  randomArticle(),
-  randomArticle()
-]
+function fetchArticle(name) {
+  fetchPage(name, {
+    success: (response) => {
+      articles.push(pageToArticle(response))
+      EventBus.broadcast('article:add', articles)
+    }
+  })
+}
+
+function search(string) {
+  performSearch(string, {
+    success: (response) => {
+      EventBus.broadcast('search:success', searchResultsToArticles(response))
+    }
+  })
+}
+
+let articles = []
 
 export default articles
